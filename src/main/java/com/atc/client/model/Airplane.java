36b5ad76ec6756e9d50e4c5339969e76577a5f6e
magdahime <magdalena.majkowska1@gmail.com>
2020-04-05 20:20:44 +0200

    Changed to '<=' in last condition in updateHeading

--- src/main/java/com/atc/client/model/Airplane.java
+++ src/main/java/com/atc/client/model/Airplane.java
@@ -78,7 +78,7 @@ public class Airplane {
             setCurrHeading(currHeading + headingStep);
         }else if (difference < 0 && Math.abs(difference) > headingStep){
             setCurrHeading((currHeading - headingStep));
-        }else if ((360 - Math.abs(difference)) < headingStep){
+        }else if ((360 - Math.abs(difference)) <= headingStep){
             setCurrHeading(getTargetHeading());
         }
     }
